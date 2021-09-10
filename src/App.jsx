c85074c556b9b371da6f05c469d116d779ed4a91
Mario Conti <marioConti91@gmail.com>
2021-09-10 10:30:17 -0300

    se agregan componentes del portfolio y archivos css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Container } from "./container/container";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <Container />
+    </div>
+  );
 }
 
 export default App;
